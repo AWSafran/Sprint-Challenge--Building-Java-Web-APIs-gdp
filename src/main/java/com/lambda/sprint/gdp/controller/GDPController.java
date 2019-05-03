1f5be3675ad6fb609294e9d667d3139b8b255f78
AWSafran <andrewsafran@gmail.com>
2019-05-03 13:11:47 -0400

    logging endpoint access to console with date and time

--- src/main/java/com/lambda/sprint/gdp/controller/GDPController.java
+++ src/main/java/com/lambda/sprint/gdp/controller/GDPController.java
@@ -4,6 +4,8 @@ import com.lambda.sprint.gdp.GdpApplication;
 import com.lambda.sprint.gdp.exception.ResourceNotFoundException;
 import com.lambda.sprint.gdp.model.GDP;
 import com.lambda.sprint.gdp.model.GDPList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,14 +18,15 @@ import java.util.ArrayList;
 @RestController
 public class GDPController
 {
+    private static final Logger logger = LoggerFactory.getLogger(GDPController.class);
+    
     //localhost:2019/names
     @GetMapping(value="/names", produces = {"application/json"})
     public ResponseEntity<?> getAllNames()
     {
-        
+        logger.info("/names has been accessed");
         ArrayList<GDP> allNames = GdpApplication.ourList.gdpList;
         allNames.sort((g1, g2) -> g1.getCountry().compareToIgnoreCase(g2.getCountry()));
-        System.out.println("accessed endpoint");
         
         return new ResponseEntity<>(allNames, HttpStatus.OK);
     }
@@ -32,6 +35,7 @@ public class GDPController
     @GetMapping(value ="/economy", produces = {"application/json"})
     public ResponseEntity<?> getEconomy()
     {
+        logger.info("/economy has been accessed");
         ArrayList<GDP> allEconomies = GdpApplication.ourList.gdpList;
         allEconomies.sort((g1, g2) -> (int)(g2.getGdp() - g1.getGdp()));
         return new ResponseEntity<>(allEconomies, HttpStatus.OK);
@@ -41,6 +45,7 @@ public class GDPController
     @GetMapping(value="/gdp/{country}", produces = {"application/json"})
     public ResponseEntity<?> getCountry(@PathVariable String country)
     {
+        logger.info("/gdp/{country} has been accessed with parameter: " + country);
         GDP selectedCountry = GdpApplication.ourList.findCountry(c -> c.getCountry().equalsIgnoreCase(country));
         
         if(selectedCountry == null)
@@ -56,6 +61,7 @@ public class GDPController
     @GetMapping(value="/country/{id}", produces = {"application/json"})
     public ResponseEntity<?> getCountryById(@PathVariable long id)
     {
+        logger.info("/country/{id} has been accessed with parameter: " + id);
         GDP selectedCountry = GdpApplication.ourList.findCountry(c -> c.getId() == id);
         if(selectedCountry == null)
         {
@@ -70,6 +76,7 @@ public class GDPController
     @GetMapping(value="/country/stats/median", produces={"application/json"})
     public ResponseEntity<?> getMedianGdp()
     {
+        logger.info("/country/stats/median has been accessed");
         ArrayList<GDP> orderedCountries = GdpApplication.ourList.gdpList;
         orderedCountries.sort((c1, c2) -> (int)(c2.getGdp() - c1.getGdp()));
         
