4a193e20429af79f23e7a0538781af62e61e4467
AWSafran <andrewsafran@gmail.com>
2019-05-03 12:01:19 -0400

    added id field to GDP using atomiclong

--- src/main/java/com/lambda/sprint/gdp/model/GDP.java
+++ src/main/java/com/lambda/sprint/gdp/model/GDP.java
@@ -1,12 +1,17 @@
 package com.lambda.sprint.gdp.model;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class GDP
 {
+    private static AtomicLong counter = new AtomicLong();
+    private long id;
     private String country;
     private long gdp;
     
     public GDP(String country, String gdp)
     {
+        id = counter.incrementAndGet();
         this.country = country;
         this.gdp = Long.parseLong(gdp);
     }
