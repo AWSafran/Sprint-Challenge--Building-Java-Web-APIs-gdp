8d4792e6d29fa8f689ecf99a6aaae95c6e5b20df
AWSafran <andrewsafran@gmail.com>
2019-05-03 11:34:18 -0400

    added jackson, actuator, and slf4j dependencies to pom.xml

---
+++ src/main/java/com/lambda/sprint/gdp/CheckCountry.java
@@ -0,0 +1,8 @@
+package com.lambda.sprint.gdp;
+
+import com.lambda.sprint.gdp.model.GDP;
+
+public interface CheckCountry
+{
+    boolean test(GDP g);
+}
--- src/main/java/com/lambda/sprint/gdp/model/GDP.java
+++ src/main/java/com/lambda/sprint/gdp/model/GDP.java
@@ -2,4 +2,32 @@ package com.lambda.sprint.gdp.model;
 
 public class GDP
 {
+    private String country;
+    private long gdp;
+    
+    public GDP(String country, String gdp)
+    {
+        this.country = country;
+        this.gdp = Long.parseLong(gdp);
+    }
+    
+    public String getCountry()
+    {
+        return country;
+    }
+    
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+    
+    public long getGdp()
+    {
+        return gdp;
+    }
+    
+    public void setGdp(long gdp)
+    {
+        this.gdp = gdp;
+    }
 }
